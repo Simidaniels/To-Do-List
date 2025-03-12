8d928928fcb4b6ae950546ccbe6051a3a67af581
Adepitan Daniel <simidaniels0506@gmail.com>
2025-03-12 14:42:57 +0100

    changes made

--- src/Home.js
+++ src/Home.js
@@ -1,228 +1,166 @@
-import React from 'react';
-import { useState } from 'react';
-
-
-
+import React, { useState } from 'react';
 
 function TodoList() {
-  const [items, setItems] = useState([]);         //The current state
-  const [newItem, setNewItem] = useState('');     //Function to  update the current state
+  const [items, setItems] = useState([]); // Current state for task names
+  const [newItem, setNewItem] = useState(''); // Input for new task name
 
-
-  const [text, setText] = useState([]);
-  const [newText, setNewText] = useState('');
-
-
-
-
-  //Add and Delete statement,l., 
-const addItem = () => {
-  if (newItem.trim() !== '') {
-    setItems([...items, newItem]);
-    setNewItem('');
-  }
-};
-//Add function
-const deleteItem = (index) => {
-  const updatedItems = [...items];
-  updatedItems.splice(index, 1);
-  setItems(updatedItems);
-}
-//Delete function
-
-
-const addText = () => {
-  if (newText.trim() !== '') {
-    setText([...text, newText]);
-    setNewText('');
-  }
-};
-// eslint-disable-next-line no-unused-vars
-const deleteText = (index) => {
-  const updatedText = [...text];
-  updatedText.splice(index, 1);
-  setText(updatedText);
-};
-
-
-const handleBothClicks = () => {
-  addItem();
-  addText();
-};
-
-
-
+  const [descriptions, setDescriptions] = useState([]); // Task descriptions
+  const [newText, setNewText] = useState(''); // Input for new task description
 
   const [showInput, setShowInput] = useState(false);
-  // const [] = useState('');
-  const toggleInputVisibility = () => {
-    setShowInput(!showInput);
-  };
-  const handleInputBlur = () => {
-    setShowInput(false);
-  }
+  const [visibleIndex, setVisibleIndex] = useState(null); // Tracks which description is visible
 
-
-
-
-  const [isVisible, setIsVisible] = useState(false);
-
-  const toggleVisibility = () => {
-    setIsVisible(!isVisible);
+  // Function to add a task
+  const addItem = () => {
+    if (newItem.trim() !== '' && newText.trim() !== '') {
+      setItems([...items, newItem]);
+      setDescriptions([...descriptions, newText]);
+      setNewItem('');
+      setNewText('');
+    }
   };
 
-   const handleVisibilityBlur = () => {
-    setIsVisible(false);
-  }
+  // Function to delete a task
+  const deleteItem = (index) => {
+    const updatedItems = items.filter((_, i) => i !== index);
+    const updatedDescriptions = descriptions.filter((_, i) => i !== index);
+    setItems(updatedItems);
+    setDescriptions(updatedDescriptions);
+  };
+
+  // Toggles visibility for each description
+  const toggleVisibility = (index) => {
+    setVisibleIndex(visibleIndex === index ? null : index);
+  };
 
 
 
 
-  // const addTimeDate = () => {
-  //   const currentDate = new Date();
-  //   const dateTimeString = currentDate.toLocaleString();
-  //   setInputValue(prevValue => prevValue + dateTimeString + '\n');
-  // };
-return (      //JSX function
-<div className='content-container'>
-  <div className='content-wrapper'>
-    <div className='main-context'>
-        <h1 className='header'>Hello, <span>Daniel</span></h1>
-        <p className='hd-txt'>Lorem ipsum dolor sit amet consecteur. Aliquam habitat aliquam non alrticies nini parasent ipsumsum. consequat in integer orci ultrices nibh mattis. Ultrices at</p>          
-      <div>
-       <button 
-            className='task'
-            onClick={toggleInputVisibility}>Create Task</button>        {/* onClick={addItem} */}
-            {/* {showInput && (
-              <input
-                type='text'
-                value={inputValue}
-                onChange={handleInputChange}
-                onBlur={handleInputBlur}
-                />
-            )} */}
+
+
+
+
+
+
+
+
+  // const editItem = (index) =>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return (
+    <div className="content-container">
+      <div className="content-wrapper">
+        <div className="main-context">
+          <h1 className="header">
+            Hello, <span>Daniel</span>
+          </h1>
+          <p className="hd-txt">
+            Lorem ipsum dolor sit amet consectetur. Aliquam habitat aliquam non
+            aliquet. Consequat in integer orci ultrices nibh mattis.
+          </p>
+
+          <button className="task" onClick={() => setShowInput(!showInput)}>
+            Create Task
+          </button>
+        </div>
+
+        <div className="img-wrapper">
+          <img src={require('./dread.png')} alt="Profile" className="img" />
+        </div>
       </div>
-    </div>
-    <div className='img-wrapper'>
-        <img src={require('./dread.png')} alt='' className='img' />
-    </div>
-    </div>
 
-
-
-
-{showInput && (
-<div className='inputs-wrapper'>
-        <p>Task Name</p>
-              <textarea
-                className='task-name'
-                type='text'
-                value={newItem}
-                onChange={(e) => setNewItem(e.target.value)}
-                maxLength={25}
-                />
+      {showInput && (
+        <div className="inputs-wrapper">
+          <p>Task Name</p>
+          <textarea
+            className="task-name"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            maxLength={25}
+          />
 
           <p>Task Description</p>
-        <textarea
-             className='task-description fill'
-             type="text"
-            //  placeholder='Type an item'
+          <textarea
+            className="task-description fill"
             value={newText}
             onChange={(e) => setNewText(e.target.value)}
             maxLength={80}
-          rows={4}
+            rows={4}
             cols={50}
           />
 
-
-
- 
-            <p>
-              <button 
-                className='task' 
-                onClick={handleBothClicks}
-                onBlur={handleInputBlur}
-                >Create Task</button>
-            </p>
-  </div>
- )}
-
-<div className='task-wrapper'>
-  <table>
-    <thead>
-      <tr>
-        <th className='sub_header'>Tasks</th>
-      </tr>  
-    </thead>
-
-
-
-
-    <tbody className='output-container'>
-      {items.map((item, index) => (
-        <tr key={index}>
-
-
-        <div className='para'>
-        <div onClick={toggleVisibility}>
-          <td className='content'>
-            {item}
-          </td>
+          <p>
+            <button className="task" onClick={addItem}>
+              Create Task
+            </button>
+          </p>
         </div>
-           <button className='dlt_btn' onClick={() => deleteItem()}><img src={require('./trash.png')} alt='' className='icn'/></button>
+      )}
+
+      <div className="task-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th className="sub_header">Tasks</th>
+            </tr>
+          </thead>
+
+          <tbody className="output-container">
+            {items.map((item, index) => (
+              <tr className='para' key={index}>
+                <td className="content" onClick={() => toggleVisibility(index)}>
+                  {item}
+                </td>
+                <td>
+                  <button className="dlt_btn" onClick={() => deleteItem(index)}>
+                    <img src={require('./trash.png')} alt="Delete" className="icn" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      {visibleIndex !== null && (
+        <div className="description-wrapper">
+          <p>Description</p>
+          <div className="dsc-fld">{descriptions[visibleIndex]}</div>
+
+          <p>
+            <button className="close-btn" onClick={() => setVisibleIndex(null)}>
+              Close
+            </button>
+          </p>
         </div>
-
-
-
-        </tr>
-      ))}
-    </tbody>
-
-
-
-        
-
-
-
-{isVisible && (
-  <div className='description-wrapper'>
-    
-        <p>Description</p>
-            <div
-              
-              className='dsc-fld'>{text}</div>
-
-
-            <p>
-              <button 
-              className='close-btn'
-              onBlur={toggleVisibility}
-              onClick={handleVisibilityBlur}
-                >Close</button>
-            </p>
-  </div>
-)}
-
-
-
-
-
-
-
-
-
-
-
-
-    </table> 
-</div>
-</div>
+      )}
+    </div>
   );
 }
-
 
 export default TodoList;
 
 
+
 // Home.js..........
 // Composition of the Index, Navigation, Rating....
+
+
+
+// MAKE YOUR DESRIPTION FIELD RE-EDITABLE....
